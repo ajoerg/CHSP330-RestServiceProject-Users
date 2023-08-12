3c3b6a42931d8032b97ecfede583f5280ac1b1f6
Andrew <AndrewJoerger96@gmail.com>
2023-08-12 15:24:30 -0500

    Add User model and CRUD operations to UserController

---
+++ UsersRestService/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsersRestService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsersRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public static List<User> users = new List<User>();
+
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var user = users.Where((user) => user.Id == id).First();
+                return Ok(user);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            if ((user.Email == null || user.Email == "")
+                && (user.Password == null || user.Password == ""))
+            {
+                return BadRequest("Error: Email and Password fields are missing");
+            }
+
+            if (user.Email == null || user.Email == "")
+            {
+                return BadRequest("Error: Email field is missing");
+            }
+
+            if (user.Password == null || user.Password == "")
+            {
+                return BadRequest("Error: Password field is missing");
+            }
+
+            DateTime dateTime = DateTime.Now;
+            user.DateAdded = dateTime;
+
+            int newId;
+            if(users.Count == 0)
+            {
+                newId = 1;
+            }
+            else
+            {
+                newId = users.Select(c => c.Id).ToList().Max() + 1;
+            }
+
+            user.Id = newId;
+            users.Add(user);
+
+            return CreatedAtAction(nameof(Get), new { id = newId }, user);
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User updatedUser)
+        {
+            if (updatedUser.Id == 0 || updatedUser.Id == id)
+            {
+
+                var existingUser = users.FirstOrDefault(c => c.Id == id);
+
+                if (existingUser == null)
+                {
+                    return NotFound();
+                }
+
+                existingUser.Email = updatedUser.Email;
+                existingUser.Password = updatedUser.Password;
+
+                return Ok(existingUser);
+            }
+
+            return Conflict();
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            int numberOfElementsRemoved = users.RemoveAll((user) => user.Id == id);
+            if (numberOfElementsRemoved <= 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
